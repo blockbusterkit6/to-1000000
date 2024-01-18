f626e19148b53ff20d39b1815559e82e18b01f4c
blockbusterkit6 <47342798+blockbusterkit6@users.noreply.github.com>
2024-01-18 11:08:16 -0600

    Add files via upload

--- go to 100000.py
+++ go to 100000.py
@@ -1,5 +1,8 @@
 spam = 0
-while spam < 100001:
+while spam < 100001:#is 100001 so it will count to 100000
     print(spam)
     spam = spam + 1
-print('Done whent and did your mom!')
+print('All done!')
+print('press enter when done')
+spam = input()#just so window dose not close
+
--- go to 1000000.py
+++ go to 1000000.py
@@ -1,5 +1,7 @@
 spam = 0
-while spam < 1000001:
+while spam < 1000001:#is 1000001 so it will count to 1000000
     print(spam)
     spam = spam + 1
-print('Done whent and did your mom!')
+print('All done!')
+print('press enter when done')
+spam = input()#just so window dose not close
