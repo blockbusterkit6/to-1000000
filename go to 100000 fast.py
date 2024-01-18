4e69f807dba8bbc4a175aeb75343e3d05f117545
blockbusterkit6 <47342798+blockbusterkit6@users.noreply.github.com>
2024-01-18 12:10:54 -0600

    Add files via upload
    all numbers on same line so much faster

---
+++ go to 100000 fast.py
@@ -0,0 +1,8 @@
+spam = 0
+while spam < 100001:#is 100001 so it will count to 100000
+    print(spam, end=' ')
+    spam = spam + 1
+print('All done!')
+print('press enter when done')
+spam = input()#just so window dose not close
+
---
+++ go to 1000000 fast.py
@@ -0,0 +1,7 @@
+spam = 0
+while spam < 1000001:#is 1000001 so it will count to 1000000
+    print(spam, end=' ')
+    spam = spam + 1
+print('All done!')
+print('press enter when done')
+spam = input()#just so window dose not close
